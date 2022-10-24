1ee3359bfbb80b1ac6309d16bd2dad5b76aee53a
dogmash69 <103027893+dogmash69@users.noreply.github.com>
2022-10-24 09:55:24 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'QuestAngu';
+  title = 'Project Yvens: MAKE HIM WORK';
 }
